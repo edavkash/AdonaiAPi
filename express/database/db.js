7c5d36b4dae29dea2fc6507da44c6fef453e1ea2
David <davidkashemwa19@gmail.com>
2024-08-21 19:02:53 +0200

    new committe

--- express/database/db.js
+++ express/database/db.js
@@ -5,25 +5,25 @@ const schema = new Schema([
     name: {
       type: String,
       required: true,
-      //minlength: 3,
-      //maxlength: 30,
+      minlength: 3,
+      maxlength: 30,
     },
     age: {
       type: Number,
       required: true,
-     // min: 16,
-      //max: 100,
+      min: 16,
+      max: 100,
     },
 
     email: {
       type: String,
       required: true,
-      //minlength: 10,
-      //maxlength: 100,
+      minlength: 10,
+      maxlength: 100,
     },
   },
 ]);
 
-const Model = new model("Collection", schema);
+const Model = new model("Collections", schema);
 
 export default Model;
